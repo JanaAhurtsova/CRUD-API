491d2772748d578ca0631ecb60f37877c1601fff
Jana Ahurtsova <byjankaaa@gmail.com>
2023-07-03 18:53:23 +0300

    fix: update config and add 1 test

--- tests/errors-server.test.ts
+++ tests/errors-server.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { StatusCodes } from '../src/constants/codes/Codes';
 import { errorMessages } from '../src/constants/errors/Errors';
 import { server } from '../src/server/Server';
-import { mockBody, mockId } from './constants';
+import { mockBody, mockId, mockUpdate } from './constants';
 
 jest.mock('uuid', () => ({ v4: () => mockId }));
 
@@ -22,9 +22,15 @@ describe('check server errors', () => {
     expect(res.body.message).toBe(errorMessages.Invalid_Endpoint);
   });
 
-  it('should get server error', async () => {
+  it('should get server error by sending get request', async () => {
     const userById = await request(server).get(`/api/users/${mockId}`);
     expect(userById.statusCode).toBe(StatusCodes[500]);
     expect(userById.body.message).toEqual(errorMessages.Server_Error);
+  });
+
+  it('should get server error by sending put request', async () => {
+    const updatedUser = await request(server).put(`/api/users/${mockId}`).send(mockUpdate);
+    expect(updatedUser.statusCode).toBe(StatusCodes[500]);
+    expect(updatedUser.body.message).toEqual(errorMessages.Server_Error);
   });
 });
